65e1e781a1d69c78e00ca1aa8159b1ff3c75c851
Ianej <Ianej1@ya.ru>
2012-05-01 19:56:29 +0400

    Strnicy grupp

--- app/controllers/grupy_controller.rb
+++ app/controllers/grupy_controller.rb
@@ -1,11 +1,14 @@
 class GrupyController < ApplicationController
   def spisok_g
+    @title="Spisok grupp"
   end
 
   def novaia_g
+    @title="Novaia gruppa"
   end
 
   def sostav_g
+    @title="Sostav gruppy"
   end
 
 end
